efb3554173fae53a936015421a3adfe6f4970069
Serafimos134-del <Serafimos134@gmail.com>
2026-06-18 12:18:44 +0300

    Update keyboards.py

--- core/keyboards.py
+++ core/keyboards.py
@@ -3,4 +3,5 @@ from telegram import ReplyKeyboardMarkup
 BTN_CANCEL = "❌ Отмена"
 
 def cancel_keyboard():
-    return ReplyKeyboardMarkup([[BTN_CANCEL]], resize_keyboard=True)
+    return ReplyKeyboardMarkup([[BTN_CANCEL]],
+    resize_keyboard=True)
